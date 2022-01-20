77644f712ddc57c542893ccf0b4e024359a297ed
Aditya <gvaditya2000@gmail.com>
2022-01-20 19:21:10 +0530

    initial commit

---
+++ Insertion_sort.py
@@ -0,0 +1,18 @@
+arr=[int(x) for x in input().split()]
+i=0
+while(i<len(arr)):
+    sum1=0
+    flag=False
+    j=i-1
+    while(j>=0):
+        if arr[i]<arr[j]:
+            flag=True
+            sum1=j
+        j=j-1    
+    if (flag):
+        arr[i],arr[sum1]=arr[sum1],arr[i]
+        i=sum1+1
+    else:
+        i=i+1
+print(arr)
+
